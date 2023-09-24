a3b68afe56dc5d9f6da8d9c3ed07fd8752d2647f
MaheshTG <mahi1546@gmail.com>
2023-09-24 21:04:48 +0530

    addd classes

--- bankService/bankService/src/main/java/com/app/bank/model/Account.java
+++ bankService/bankService/src/main/java/com/app/bank/model/Account.java
@@ -1,6 +1,11 @@
 package com.app.bank.model;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -8,5 +13,17 @@ import lombok.Data;
 @Entity
 @Table(name="account_details")
 public class Account {
-
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer accountId;
+	private String accountName;
+	private String accountSurName;
+	private String city;
+	private String email;
+	private String password;
+	private String phNum;
+	private String accoutNum;
+	private Date accountDate;
+	private Integer balance;
 }
--- bankService/bankService/src/main/java/com/app/bank/model/Transaction.java
+++ bankService/bankService/src/main/java/com/app/bank/model/Transaction.java
@@ -1,6 +1,11 @@
 package com.app.bank.model;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -8,5 +13,13 @@ import lombok.Data;
 @Entity
 @Table(name="Transactions")
 public class Transaction {
-
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer tansactionId;
+	private Date transactionDate;
+	private String fromAccount;
+	private String toAccount;
+	private String amount;
+	private String description;
 }
